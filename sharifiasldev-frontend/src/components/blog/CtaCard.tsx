682a1d396364de531675fb5b6f3d2a3b9f828ce6
Burserk84 <amirburserk@gmail.com>
2025-09-20 07:31:29 +0330

    fix: cta color mismatch

--- sharifiasldev-frontend/src/components/blog/CtaCard.tsx
+++ sharifiasldev-frontend/src/components/blog/CtaCard.tsx
@@ -2,15 +2,16 @@ import { Button } from "@/components/ui/Button";
 export default function CtaCard() {
   return (
     <div className="bg-orange-400 p-6 rounded-lg text-right text-gray-900">
-      <h3 className="text-2xl font-bold text-white">
+      <h3 className="text-2xl font-bold">
         آیا برای پروژه‌ی خود به کمک نیاز دارید؟
       </h3>
-      <p className="mt-2 text-gray-400 max-w-xl mx-auto">
+      <p className="mt-2 max-w-xl mx-auto">
         تیم ما در Devora آماده است تا ایده‌های شما را به راه‌حل‌های دیجیتال
         قدرتمند تبدیل کند.
       </p>
       <div className="mt-6">
         <Button
+          className="w-full"
           href="https://devorastudio.ir"
           variant="primary"
           size="lg"
