f5f68b23c3620bef152b928cc2b90cd73e5a1f84
Burserk84 <amirburserk@gmail.com>
2025-09-30 20:41:28 +0330

    update metadata for favico

--- sharifiasldev-frontend/src/app/layout.tsx
+++ sharifiasldev-frontend/src/app/layout.tsx
@@ -8,10 +8,32 @@ const vazirmatn = Vazirmatn({ subsets: ["latin", "arabic"] });
 
 export const metadata = {
   title: "Amirali Sharifi Asl | Full-Stack Developer",
-  description: "Full-Stack Developer specializing in building modern web applications with Next.js, Node.js, and TypeScript.",
+  description:
+    "Full-Stack Developer specializing in building modern web applications with Next.js, Node.js, and TypeScript.",
+
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      {
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      { url: "/favicon.ico" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+    },
+  },
+  manifest: "/site.webmanifest",
+
   openGraph: {
     title: "Amirali Sharifi Asl | Full-Stack Developer",
-    description: "Full-Stack Developer specializing in building modern web applications with Next.js, Node.js, and TypeScript.",
+    description:
+      "Full-Stack Developer specializing in building modern web applications with Next.js, Node.js, and TypeScript.",
     url: "https://www.sharifiasldev.ir",
     siteName: "Amirali Sharifi Asl Portfolio",
     images: [
@@ -39,9 +61,9 @@ export default function RootLayout({
       >
         {/* 2. Wrap the contents of your body with the Providers component */}
         <Providers>
-            <Header />
-            <main className="flex-grow">{children}</main>
-            <Footer />
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
         </Providers>
       </body>
     </html>
